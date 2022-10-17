3af50886240e58aff4bcf7cf1ca08091628292b0
KDQI <kasperuljas@gmail.com>
2022-10-17 18:06:28 +0300

    Enemy + Enemy Animations added

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour
+{
+	public static MainCamera instance;
+	public float interpVelocity;
+	public float minDistance;
+	public float followDistance;
+	public GameObject target;
+	public static bool isFollowingPlayer;
+	public Vector3 offset;
+	public Vector3 targetPos;
+	private Vector3 minBounds, maxBounds;
+	private float halfHeight, halfWidth;
+	public BoxCollider2D boundsBox;
+
+
+
+	void Start()
+	{
+		instance = this;
+		isFollowingPlayer = true;
+		targetPos = transform.position;
+		maxBounds = boundsBox.bounds.max;
+		minBounds = boundsBox.bounds.min;
+		halfHeight = Camera.main.orthographicSize;
+		halfWidth = halfHeight * Screen.width / Screen.height;
+	}
+
+
+	void FixedUpdate()
+	{
+		if (target && isFollowingPlayer)
+		{
+			Vector3 posNoZ = transform.position;
+			posNoZ.z = target.transform.position.z;
+
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+
+			interpVelocity = targetDirection.magnitude * 5f;
+
+			targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+			transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+		}
+	}
+
+	private void LateUpdate()
+	{
+		float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+		float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+		transform.position = new Vector2(clampedX, clampedY);
+	}
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    public bool playerCanMove;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerCanMove = true;
+
+    }
+    void Update()
+    {
+        if (playerCanMove == true)
+        {
+            ProcessInputs();
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (playerCanMove == true)
+        {
+            Move();
+            Turn();
+        }
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * Speed, moveDirection.y * Speed);
+    }
+    void Turn()
+    {
+        Vector3 mouse = Input.mousePosition;
+
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
+
+}
+
